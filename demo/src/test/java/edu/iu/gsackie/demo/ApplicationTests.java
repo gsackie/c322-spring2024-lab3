1d3d82876937929a6e1778d53fa2e8219fb7cff9
gsack <gsackie@iu.edu>
2024-01-26 11:57:01 -0500

    Initial commit

---
+++ demo/src/test/java/edu/iu/gsackie/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.gsackie.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
